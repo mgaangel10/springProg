55058b2182ee03199aa016b6cccb52f542364720
miguel angel <mg.aangel10@gmail.com>
2023-04-17 10:22:04 +0200

    actualizacion

---
+++ demo-datajpa/src/main/java/com/salesianostriana/EjemploExplicacionSpringProg/Alumno.java
@@ -0,0 +1,5 @@
+package com.salesianostriana.EjemploExplicacionSpringProg;
+
+public class Alumno {
+
+}
---
+++ demo-datajpa/src/main/java/com/salesianostriana/EjemploExplicacionSpringProg/DemoDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.EjemploExplicacionSpringProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDatajpaApplication.class, args);
+	}
+
+}
