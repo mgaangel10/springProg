d3b52fff8810b67e7117df92c2458dded06dc883
miguel angel <mg.aangel10@gmail.com>
2023-05-05 14:28:43 +0200

    html se ve ya

--- ProyectoFinalPrueba/src/main/resources/data.sql
+++ ProyectoFinalPrueba/src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Luis Miguel','Lopez','luismi.lopez@triana.com');
-insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Angel','Naranjo','angel.narajo@triana.com');
+insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Jose','Ruiz','luismi.jose@triana.com');
+insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Angel','perez alvarez','angel.narajo@triana.com');
+insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Raul','contreras garcia','raul.narajo@triana.com');
+insert into empleado (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Adrian','fernandez leal','adrian.narajo@triana.com');
