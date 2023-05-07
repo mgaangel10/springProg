f98395a866c2d30d80507cdc95c7c128ad3d8f58
mgaangel10 <mg.aangel10@gmail.com>
2023-05-08 00:15:47 +0200

    avance considereable

--- ProyectoFinalPrueba/src/main/java/com/salesianostriana/dam/Empleado/controller/EmpleadoController.java
+++ ProyectoFinalPrueba/src/main/java/com/salesianostriana/dam/Empleado/controller/EmpleadoController.java
@@ -1,8 +1,18 @@
 package com.salesianostriana.dam.Empleado.controller;
 
+import java.awt.PageAttributes.MediaType;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.Empelado.model.Empleado;
 import com.salesianostriana.dam.Empleado.service.EmpleadoService;
@@ -16,16 +26,32 @@ public class EmpleadoController {
 		this.emse=ser;
 	}
 	@GetMapping({"/","/todo"})
-	public String Mostrar(Model model) {
-		Empleado e=new Empleado();
-		model.addAttribute("lista",emse.findAll());
+	public String index(Model model) {
 		return "index";
 	}
-	
+	@GetMapping("/mostrarFormulario")
+	public String Mostrar(Model model) {
+		model.addAttribute("lista",emse.findAll());
+		return "vistaFormularioTerminado";
+	}
+	@GetMapping("/nuevo")
 	public String mostrarFormulario(Model model) {
 		model.addAttribute("empleado",new Empleado());
-		return "Formulario";
+		return "vistaFormulario";
 	}
+	@PostMapping("/nuevo/submit")
+	public String procesarFormulario(@ModelAttribute("empleado") Empleado e) {
+		emse.add(e);
+		return "redirect:/mostrarFormulario";
+	}
+	
+	
+	
+	
+	
+	
+
+	
 	
 	
 	}
